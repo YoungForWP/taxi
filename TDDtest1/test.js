7f31553e8a79046e18627648454cf3cdc6d2a0d3
wangpu <1262240943@qq.com>
2017-04-13 16:18:11 +0800

    add test the number is even

---
+++ TDDtest1/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wangpu on 17-4-13.
+ */
+descripe('', () => {
+    it('should return isEven if input is a even number', () =>{
+       const actualValue = checkString(2);
+       expect(actualValue).toBe('isEven');
+    });
+})
