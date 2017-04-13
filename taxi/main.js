cdee49d3b8456516751eda8863e3611659f998ac
wangpu <1262240943@qq.com>
2017-04-13 22:07:36 +0800

    Reformat Code

--- taxi/main.js
+++ taxi/main.js
@@ -1,15 +1,15 @@
-function calculateTaxiFee(distance,waitMinute){
-    let fee = 0;
-    if(distance < 0){
+function calculateTaxiFee(distance, waitMinute) {
+    if (distance < 0) {
         return 'Input Error!';
     }
+    let fee = 0;
     const waitFee = 0.25 * waitMinute;
-    if(distance < 2){
+    if (distance < 2) {
         fee = Math.round(6 + waitFee);
-    }else if(distance <= 8){
-        fee = Math.round(distance*0.8 + waitFee);
-    }else if(distance > 8){
-        fee = Math.round(6.4 + (distance-8)*1.2 + waitFee);
+    } else if (distance <= 8) {
+        fee = Math.round(distance * 0.8 + waitFee);
+    } else if (distance > 8) {
+        fee = Math.round(6.4 + (distance - 8) * 1.2 + waitFee);
     }
     return fee;
 }
--- taxi/test.js
+++ taxi/test.js
@@ -3,22 +3,27 @@
  */
 describe('test taxi price', () => {
     it('should return 9 yuan if input distance < 2 km and waitMinute is 13 min', () => {
-        const result = calculateTaxiFee(1,13);
+        const result = calculateTaxiFee(1, 13);
         expect(result).toBe(9);
     });
 
     it('should return 11 yuan if input distance big than 2 km but less than 8 km', () => {
-        const result = calculateTaxiFee(6,25);
+        const result = calculateTaxiFee(6, 25);
         expect(result).toBe(11);
     });
 
     it('should return 18 yuan if input distance big than 8 km', () => {
-        const result = calculateTaxiFee(9.2,40);
+        const result = calculateTaxiFee(9.2, 40);
         expect(result).toBe(18);
     });
 
     it('should return error if input distance less than 0 km', () => {
-        const result = calculateTaxiFee(-1,10);
+        const result = calculateTaxiFee(-1, 10);
+        expect(result).toBe('Input Error!');
+    });
+
+    it('should return error if input distance is invalid string', () => {
+        const result = calculateTaxiFee(-1, 10);
         expect(result).toBe('Input Error!');
     });
 });
