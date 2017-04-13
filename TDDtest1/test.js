8c11f64fcc5e15bd3de785c3043894cc24f0cbef
wangpu <1262240943@qq.com>
2017-04-13 16:20:25 +0800

    add test the input is odd

--- TDDtest1/test.js
+++ TDDtest1/test.js
@@ -2,8 +2,15 @@
  * Created by wangpu on 17-4-13.
  */
 descripe('', () => {
+
     it('should return isEven if input is a even number', () =>{
        const actualValue = checkString(2);
        expect(actualValue).toBe('isEven');
     });
+
+    it('should return isOdd if input is odd number', () =>{
+        const actualValue = checkString(3);
+        expect(actualValue).toBe('isOdd');
+    });
+    
 })
